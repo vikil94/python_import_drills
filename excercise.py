120550a1e15af924ef0a1ad53650dc633feed17e
Vikil Naik <vikilnaik37@gmail.com>
2019-02-19 13:32:51 -0500

    Tested out all of the images in this file

---
+++ excercise.py
@@ -0,0 +1,14 @@
+from ascii import one, two
+print("this is number 1: {}".format(one()))
+print("this is number 2: {}".format(two()))
+#go into a folder and grab a file
+
+from art.pictures import three, four
+print("this is number 3: {}".format(three()))
+print("this is number 4: {}".format(four()))
+
+from art.even_more_art.images import five
+print("this is number 5: {}".format(five()))
+
+from scene import six
+print("this is number 6: {}".format(six()))
